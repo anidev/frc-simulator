bce86273222c1da1b5b7f47a84c5bd5aa9217f2d
anidev <anidev.aelico@gmail.com>
2013-01-26 19:27:02 -0500

    Added implementations to tInterruptManager and work with interrupts

--- frc-cpp-sim/crio/fpga/src/tInterruptManager.cpp
+++ frc-cpp-sim/crio/fpga/src/tInterruptManager.cpp
@@ -5,33 +5,50 @@
 /*----------------------------------------------------------------------------*/
 
 #include <pthread.h>
+#include "interfaces/NiFpga.h"
 #include "interfaces/NiRio.h"
 #include "interfaces/tInterruptManager.h"
 #include "interrupts.h"
 
+struct manager_data {
+	bool enabled;
+	nFPGA::tInterruptHandler handler;
+	void* param;
+};
+
 void interrupt_helper(unsigned int mask,void* param) {
-	tInterruptManager* manager=(tInterruptManager*)param;
-	if(manager->_enabled) {
-		manager->_handler(manager->_userParam);
+	manager_data* data=(manager_data*)param;
+	if(data->enabled&&data->handler!=NULL&&data->param!=NULL) {
+		data->handler(mask,data->param);
 	}
 }
 
 namespace nFPGA {
 
-tInterruptManager::tInterruptManager(unsigned int mask,bool watcher,tRioStatusCode* status):_interruptMask(mask),
-                                                                                            _watcher(watcher),
+tInterruptManager::tInterruptManager(unsigned int mask,bool watcher,tRioStatusCode* status):tSystem(status),
                                                                                             _handler(NULL),
+                                                                                            _interruptMask(mask),
+                                                                                            _watcher(watcher),
                                                                                             _userParam(NULL)
 {
 	disable(NULL);
+	manager_data* data=new manager_data;
+	data->enabled=false;
+	data->handler=NULL;
+	data->param=NULL;
+	interrupts_callback callback={interrupt_helper,(void*)_userParam};
+	interrupts::get_instance()->register_callback(mask,callback);
 }
 
 tInterruptManager::~tInterruptManager() {
 }
 
-tInterruptManager::registerHandler(tInterruptHandler handler,void* param,tRioStatusCode* status) {
+void tInterruptManager::registerHandler(tInterruptHandler handler,void* param,tRioStatusCode* status) {
 	_handler=handler;
 	_userParam=param;
+	manager_data* data=(manager_data*)_userParam;
+	data->handler=handler;
+	data->param=param;
 	if(_enabled) {
 		enable(NULL);
 	} else {
@@ -39,5 +56,26 @@ tInterruptManager::registerHandler(tInterruptHandler handler,void* param,tRioSta
 	}
 }
 
+unsigned int tInterruptManager::watch(int timeout,tRioStatusCode* status) {
+	// TODO
+	status=(tRioStatusCode*)&NiFpga_Status_IrqTimeout;
+	return 0;
+}
+
+void tInterruptManager::enable(tRioStatusCode* status) {
+	_enabled=true;
+	manager_data* data=(manager_data*)_userParam;
+	data->enabled=true;
+}
+
+void tInterruptManager::disable(tRioStatusCode* status) {
+	_enabled=false;
+	manager_data* data=(manager_data*)_userParam;
+	data->enabled=false;
+}
+
+bool tInterruptManager::isEnabled(tRioStatusCode* status) {
+	return _enabled;
+}
 
 }
