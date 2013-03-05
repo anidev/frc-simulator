44dc815ef7e55bd3552e74d2ae449be259747256
anidev <anidev.aelico@gmail.com>
2013-03-05 14:49:29 -0500

    Started tSystem implementation

---
+++ frc-cpp-sim/crio/fpga/src/tSystem.cpp
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) Anirudh B. (anidev) 2012. All Rights Reserved.               */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in this directory.       */
+/*----------------------------------------------------------------------------*/
+
+#include "interfaces/NiRio.h"
+#include "interfaces/tSystem.h"
+
+namespace nFPGA {
+
+tSystem::tSystem(tRioStatusCode* status) {
+}
+
+tSystem::~tSystem() {
+}
+
+}
